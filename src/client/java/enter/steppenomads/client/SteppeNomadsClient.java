bc6f68aa7045885b867e039860c978f2b16b8ead
User <toleu.era10.2004@gmail.com>
2026-07-27 00:43:42 +0500

    First commit

---
+++ src/client/java/enter/steppenomads/client/SteppeNomadsClient.java
@@ -0,0 +1,10 @@
+package enter.steppenomads.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class SteppeNomadsClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
---
+++ src/client/java/enter/steppenomads/client/SteppeNomadsDataGenerator.java
@@ -0,0 +1,11 @@
+package enter.steppenomads.client;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class SteppeNomadsDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
